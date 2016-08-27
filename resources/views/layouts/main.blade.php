16794f9ba01f4b86ee4aaabc1ae2383fa5dfe2e5
valeriy <mc-kirpi4@mail.ru>
2016-08-27 15:39:50 +0300

    Create the project.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+<div class="container">
+    <div class="content col-lg-4">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
